136622700a8262bea92596922d682d73b47a3a4b
FarKlit <farruhk91@mail.com>
2024-04-26 17:14:15 +0500

    registration

--- js/reg.js
+++ js/reg.js
@@ -1,12 +1,4 @@
-const alldomains = ["google", "yandex", "mail", "ru"]
 function validation(form) {
-    function removeError(input) {
-        const parent = input.parentNode;
-        if(parent.classList.contains('error')) {
-            parent.querySelector('.error-label').remove()
-            parent.classList.remove('error')
-        }
-    }
     function createError(text,input) {
         const parent = input.parentNode;
         const errorLabel = document.createElement('label')
@@ -18,40 +10,21 @@ function validation(form) {
     let result = true
     const allInputs = form.querySelectorAll('input')
     for (const input of allInputs) {
-        removeError(input)
         if(input.value=="") {
+            console.log("Заполните форму")
             createError("Поле не заполнено", input)
             result = false
         }
-        
-        if(input.dataset.maxLength) {
-            if(input.value.length > input.dataset.maxLength)  {
-                createError(`Максимальное количество символов не должно превышать:${input.dataset.maxLength}`, input)
-                result = false     
-            }
-        }
-
-        if(input.dataset.minLength) {
-            if(input.value.length < input.dataset.minLength) {
-                createError(`Минимальное количество символов не должно быть меньше:${input.dataset.minLength}`, input)
-                result = false  
-            }
-        }  
-
-        if(input.dataset.type) {
-            if(!input.value.includes('@')) {
-                createError(`Email должен содержать символ @`, input)
-                result = false
-            } else {
-                const domain = input.value.split('@')[1].split('.')[0]
-                if(!alldomains.includes(domain)) {
-                    createError(`Домен не поддерживается`, input)
-                    result = false
-                }
-            }
-        }
     }
 
+    if(input.dataset.maxLength) {
+        removeError(input)
+        if(input.value.length > input.dataset.maxlength) {
+            console.log("Ошибка")
+            createError(`Недопустимое количество символов:${input.dataset.maxLength}`, input)
+            result = false
+        }
+    }
     
     form.querySelectorAll('input').forEach(element => {
         console.log(element)
